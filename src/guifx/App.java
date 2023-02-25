6c143229526078f2b15aa6503f31b20866f51348
RedMugs <stephen.j.santilli@gmail.com>
2023-02-25 17:42:20 -0500

    Added SVG loader to display pieces

--- src/guifx/App.java
+++ src/guifx/App.java
@@ -1,9 +1,32 @@
 package guifx;
 
 import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.StackPane;
+
+import org.girod.javafx.svgimage.*;
 
 public class App extends Application {
 
-    
+    @Override
+    public void start(Stage stage) {
+
+        SVGImage svg = SVGLoader.load(getClass().getResource("/img/BB.svg"));
+        StackPane pane = new StackPane(svg);
+
+        Scene s = new Scene(pane, 800, 800);
+        stage.setScene(s);
+        stage.show();
+
+        // gr.fillRect(50, 50, 100, 100);
+
+    }
+
+    public static void main(String[] args) {
+
+        launch();
+
+    }
 
 }
