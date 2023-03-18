9c74e906ef28b8ae695fdc20b4c64fa8a410deec
Stephen Santilli <stephen.j.santilli@gmail.com>
2023-03-18 11:14:17 -0400

    /

--- src/main/java/game/LAN/Searcher.java
+++ src/main/java/game/LAN/Searcher.java
@@ -63,9 +63,9 @@ public class Searcher {
 
                 try {
                     Challenge add = new Challenge(packet);
-                if (!packet.getAddress().equals(ownAddress) && !hosts.contains(add))
-                    hosts.add(add);
-                } catch(Exception e) {
+                    if (!packet.getAddress().equals(ownAddress) && !hosts.contains(add))
+                        hosts.add(add);
+                } catch (Exception e) {
                     e.printStackTrace();
                     continue;
                 }
