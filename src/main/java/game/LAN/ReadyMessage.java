605dbc2e2a0e586a187c132e555e0e228f1d8491
Stephen Santilli <stephen.j.santilli@gmail.com>
2023-04-24 16:21:54 -0400

    ready

--- src/main/java/game/LAN/ReadyMessage.java
+++ src/main/java/game/LAN/ReadyMessage.java
@@ -6,8 +6,62 @@ public class ReadyMessage extends Message {
 
     private final int oppColor;
     private final String name;
+    private final String fen;
     private final long timePerSide;
     private final long timePerMove;
+
+    public ReadyMessage(int oppColor, String name, String fen, long timePerSide, long timePerMove) {
+
+        super("ready", oppColor + "", name, fen, timePerSide + "", timePerMove + "");
+
+        this.oppColor = oppColor;
+        this.name = name;
+        this.fen = fen;
+        this.timePerSide = timePerSide;
+        this.timePerMove = timePerMove;
+
+    }
+
+    public ReadyMessage(String msg) throws Exception {
+
+        super(msg);
+
+        if (args.size() != 6)
+            throw new Exception("Invalid ready message.");
+
+        try {
+            this.oppColor = Integer.parseInt(args.get(1));
+        } catch (Exception e) {
+            throw new Exception("Invalid opponent color.");
+        }
+
+        if (oppColor != Challenge.CHALLENGE_WHITE && oppColor != Challenge.CHALLENGE_BLACK)
+            throw new Exception("Invalid opponent color.");
+
+        this.name = args.get(2);
+
+        if (!name.matches(Player.NAME_REGEX))
+            throw new Exception("Invalid name.");
+
+        this.fen = args.get(3);
+
+        try {
+            this.timePerSide = Long.parseLong(args.get(4));
+        } catch (Exception e) {
+            throw new Exception("Invalid time per side.");
+        }
+
+        try {
+            this.timePerMove = Long.parseLong(args.get(5));
+        } catch (Exception e) {
+            throw new Exception("Invalid time per move.");
+        }
+
+    }
+
+    public String getFen() {
+        return fen;
+    }
 
     public int getOppColor() {
         return oppColor;
@@ -23,52 +77,6 @@ public class ReadyMessage extends Message {
 
     public long getTimePerMove() {
         return timePerMove;
-    }
-
-    public ReadyMessage(int oppColor, String name, long timePerSide, long timePerMove) {
-
-        super("ready", oppColor + "", name, timePerSide + "", timePerMove + "");
-
-        this.oppColor = oppColor;
-        this.name = name;
-        this.timePerSide = timePerSide;
-        this.timePerMove = timePerMove;
-
-    }
-
-    public ReadyMessage(String msg) throws Exception {
-
-        super(msg);
-
-        if (args.size() != 5)
-            throw new Exception("Invalid ready message.");
-
-        try {
-            this.oppColor = Integer.parseInt(args.get(1));
-        } catch (Exception e) {
-            throw new Exception("Invalid opponent color.");
-        }
-
-        if (oppColor != Challenge.CHALLENGE_WHITE && oppColor != Challenge.CHALLENGE_BLACK)
-            throw new Exception("Invalid opponent color.");
-
-        this.name = args.get(2);
-        
-        if (!name.matches(Player.NAME_REGEX))
-            throw new Exception("Invalid name.");
-
-        try {
-            this.timePerSide = Long.parseLong(args.get(3));
-        } catch (Exception e) {
-            throw new Exception("Invalid time per side.");
-        }
-
-        try {
-            this.timePerMove = Long.parseLong(args.get(4));
-        } catch (Exception e) {
-            throw new Exception("Invalid time per move.");
-        }
-
     }
 
 }
